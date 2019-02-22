0b8d582b2e5d986d736e1b25c6b621ba2dd74e6d
robbertwerker <robbertwerker@gmail.com>
2019-02-22 09:59:42 +0100

    transition times shortened

--- src/components/Product.js
+++ src/components/Product.js
@@ -103,7 +103,7 @@ const ProductWrapper = styled.div`
         font-size: 1.4rem;
         border-radius: 0.5rem 0 0 0 ;
         transform:translate(100%,100%);
-        transition: all 1s linear;
+        transition: all 0.5s linear;
     }
     .img-container:hover .cart-btn{
         transform:translate(0,0);     
