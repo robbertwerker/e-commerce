49280abbd50271c4a841a857ad26c87496e60b0d
robbertwerker <robbertwerker@gmail.com>
2019-02-25 16:46:21 +0100

    cart added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,24 +1,35 @@
 import React from "react"
+import Title from "../Title"
+import CartColumns from "./CartColumns"
+import EmptyCart from "./EmptyCart"
 import { ProductConsumer } from "../context";
+import CartList from "./CartList"
+import CartTotals from "./CartTotals"
 
 export default class Cart extends React.Component {
     render() {
         
         return (
-            <>
-            
-            <div>{this.state}</div>
-            <ProductConsumer>
-            
-            {(value)=> {
-                const {img, title, price} = value.detailProduct; 
-                return (<div>
-                <h1>{title}</h1>
-                
-                <h2>{price}</h2>
-                </div>)}}
+            <section>
+                <ProductConsumer>
+                    {value => {
+                        const { cart } = value;
+                        if (cart.length > 0) {
+                            return (
+                                <React.Fragment>
+                                    <Title name="your" title="cart"/>
+                                    <CartColumns/>
+                                    <CartList value={value}/>
+                                    <CartTotals value={value}/>
+                                </React.Fragment>
+                            )
+                        }
+                        else {
+                            return <EmptyCart />
+                        }
+                    }}
                 </ProductConsumer>
-            </>
+            </section>
             
         )
     }
--- src/components/context.js
+++ src/components/context.js
@@ -9,12 +9,12 @@ class ProductProvider extends Component {
     state = {
         products: [],
         detailProduct:detailProduct,
-        cart: [],
+        cart: storeProducts,
         modalOpen:false,
         modalProduct:detailProduct,
-        cartSubtotal:0,
-        cartTax:0,
-        cartTotal:0,
+        cartSubTotal:10,
+        cartTax:20,
+        cartTotal:30,
     };
     componentDidMount() {
         this.setProducts();
